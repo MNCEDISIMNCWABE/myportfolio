7d532864454c0c2350f63f2ffb3cfa355952308d
Mncedisi Mncwabe <67195600+MNCEDISIMNCWABE@users.noreply.github.com>
2023-08-11 19:47:34 +0200

    Update and rename get_data.py to get_and_process_data.py

--- StockPriceForecasts/get_and_process_data.py
+++ StockPriceForecasts/get_and_process_data.py
@@ -1,17 +1,5 @@
-import requests
-from datetime import datetime
 import pandas as pd
-import numpy as np
 import yfinance as yf
-from prophet import Prophet
-import pygsheets
-from google.oauth2.service_account import Credentials
-from oauth2client.service_account import ServiceAccountCredentials
-import logging
-
-start_date = '2014-01-02'
-ticker_list =  ['CLS.JO', 'GLN.JO', 'PPH.JO','WHL.JO','APN.JO','RBP.JO','PIK.JO','PPE.JO',
-                'HIL.JO','SOL.JO','EXX.JO','MCG.JO','AIL.JO','TGA.JO','SSW.JO','INL.JO']
 
 def download_data(ticker_list, start_date):
     # Download and preprocess data
@@ -23,6 +11,3 @@ def download_data(ticker_list, start_date):
     df = pd.melt(data, id_vars='Date', value_vars=ticker_list)
     df.columns = ['ds', 'ticker', 'y']
     return df
-
-
-
