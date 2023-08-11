d0e89cdba74d3cd7eab231428124b852b090cc2c
Mncedisi Mncwabe <67195600+MNCEDISIMNCWABE@users.noreply.github.com>
2023-08-11 21:50:34 +0200

    Update constants.py

--- PriceTracker/constants.py
+++ PriceTracker/constants.py
@@ -1,4 +1,4 @@
-INCREDIBLE_ENDPOINT = 'https://www.incredible.co.za/'
-SEARCH_QUERY = 'products/appliances-new-9/laundry/front-loader-washing-machines'
+incredible_endpoint = 'https://www.incredible.co.za/'
+search_query = 'products/appliances-new-9/laundry/front-loader-washing-machines'
 slack_credentials = 'https://hooks.slack.com/services/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'
 title = ":rotating_light: Price Tracker Run:"
