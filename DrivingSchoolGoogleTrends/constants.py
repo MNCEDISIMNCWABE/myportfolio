0d3aeb34d2b1a402aaea9be25d23fbf3a55675bc
Mncedisi Mncwabe <67195600+MNCEDISIMNCWABE@users.noreply.github.com>
2023-08-11 22:48:23 +0200

    Create constants.py

---
+++ DrivingSchoolGoogleTrends/constants.py
@@ -0,0 +1,5 @@
+slack_credentials = 'https://hooks.slack.com/services/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'
+title = (f":rotating_light: Driving School Search Terms - Google Trends Past 24 hours:")
+# Set the keywords and time frame for the query
+kw_list = ["driving school", "learners license", "driving license", "driving schools near me", "code 14 driving school"]
+timeframe = "now 1-d"  # Last one day from now, it can take value 1 or 7 only
