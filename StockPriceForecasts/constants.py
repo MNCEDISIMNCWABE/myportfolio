af65bb1e88369c2c3b03857cf20be3ff775f473d
Mncedisi Mncwabe <67195600+MNCEDISIMNCWABE@users.noreply.github.com>
2023-08-11 19:55:47 +0200

    Create constants.py

---
+++ StockPriceForecasts/constants.py
@@ -0,0 +1,11 @@
+slack_credentials = 'https://hooks.slack.com/services/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'
+google_service_account_file = 'bright-arc-328707-b5e2d782b48b.json'
+google_sheet_id = '1wu6wT8GwPitzhY8Yov3FspFBTd2dC6znKKNcbwP70UQ'
+google_sheet_name = 'share_price_forecast'
+bq_project_id = 'bright-arc-328707'
+bq_dataset_id = 'test'
+bq_table_id = 'stock_price_predictions'
+start_date = '2014-01-02'
+ticker_list = ['CLS.JO', 'GLN.JO', 'PPH.JO', 'WHL.JO', 'APN.JO', 'RBP.JO', 'PIK.JO', 'PPE.JO',
+               'HIL.JO', 'SOL.JO', 'EXX.JO', 'MCG.JO', 'AIL.JO', 'TGA.JO', 'SSW.JO', 'INL.JO']
+title = (f":rotating_light: Stock Price Predictions Run:")
