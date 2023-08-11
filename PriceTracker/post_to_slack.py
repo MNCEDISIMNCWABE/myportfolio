7bbb0fc6f9a7c63d291fe904b072f017f4bfd268
Mncedisi Mncwabe <67195600+MNCEDISIMNCWABE@users.noreply.github.com>
2023-08-11 21:43:48 +0200

    Create post_to_slack.py

---
+++ PriceTracker/post_to_slack.py
@@ -0,0 +1,12 @@
+import requests
+from constants import slack_credentials
+
+def post_to_slack(message, slack_credentials, title):
+    data = {
+        "icon_emoji": ":white_check_mark:",
+        "attachments": [{
+            "color": "#9733EE",
+            "fields": [{"title": title, "value": message, "short": "false"}]
+        }]
+    }
+    requests.post(slack_credentials, json=data, headers={'Content-Type': 'application/json'}, verify=True)
