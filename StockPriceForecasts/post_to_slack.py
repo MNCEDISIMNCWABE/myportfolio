706fbc3b2de95210a31a5012a01667b61b67e6ae
Mncedisi Mncwabe <67195600+MNCEDISIMNCWABE@users.noreply.github.com>
2023-08-11 20:00:45 +0200

    Create post_to_slack.py

---
+++ StockPriceForecasts/post_to_slack.py
@@ -0,0 +1,11 @@
+import requests
+
+def post_to_slack(message):
+    data = {
+        "icon_emoji": ":white_check_mark:",
+        "attachments": [{
+            "color": "#9733EE",
+            "fields": [{"title": title, "value": message, "short": "false"}]
+        }]
+    }
+    requests.post(slack_credentials, json=data, headers={'Content-Type': 'application/json'}, verify=True)
