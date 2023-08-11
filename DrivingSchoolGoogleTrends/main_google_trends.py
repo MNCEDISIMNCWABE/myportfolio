15c18bce9711b3ffd8c7fcc858c22001498ee997
Mncedisi Mncwabe <67195600+MNCEDISIMNCWABE@users.noreply.github.com>
2023-08-11 23:01:59 +0200

    Create main_google_trends.py

---
+++ DrivingSchoolGoogleTrends/main_google_trends.py
@@ -0,0 +1,19 @@
+from post_to_slack import post_to_slack
+from google_trends import get_google_trends_data
+from constants import *
+
+if __name__ == '__main__':
+    try:
+        df_regions = get_google_trends_data(file)
+
+        post_to_slack(f"""
+            :white_check_mark: Successful!
+            driving school: {df_regions['driving school'].sum()}
+            learners license: {df_regions['learners license'].sum()}
+            driving license: {df_regions['driving license'].sum()}
+            driving schools near me: {df_regions['driving schools near me'].sum()}
+            code 14 driving school: {df_regions['code 14 driving school'].sum()}
+        """, slack_credentials, title)
+    except Exception as e:
+        msg = f'Error in the script: {e}'
+        post_to_slack(msg, slack_credentials, title)
