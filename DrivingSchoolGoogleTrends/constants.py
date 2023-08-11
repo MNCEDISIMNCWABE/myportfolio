95ca11e91faf76651d59cd38baad5023d8c53d67
Mncedisi Mncwabe <67195600+MNCEDISIMNCWABE@users.noreply.github.com>
2023-08-11 22:51:51 +0200

    Update constants.py

--- DrivingSchoolGoogleTrends/constants.py
+++ DrivingSchoolGoogleTrends/constants.py
@@ -3,3 +3,7 @@ title = (f":rotating_light: Driving School Search Terms - Google Trends Past 24 
 # Set the keywords and time frame for the query
 kw_list = ["driving school", "learners license", "driving license", "driving schools near me", "code 14 driving school"]
 timeframe = "now 1-d"  # Last one day from now, it can take value 1 or 7 only
+# Export to google sheets
+file = 'python-350618-3d9367733f29.json'
+sheet_id = '1TDg0Gb_M5LFd-zOi70nmw2dyCpACqMi5qLDJTl4xxFk' 
+sheet_name = 'trends'
