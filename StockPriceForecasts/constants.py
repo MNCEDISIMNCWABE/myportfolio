1a044e4269366d942c405caac71777f631b9d18e
Mncedisi Mncwabe <67195600+MNCEDISIMNCWABE@users.noreply.github.com>
2023-08-25 08:40:04 +0200

    Update constants.py

--- StockPriceForecasts/constants.py
+++ StockPriceForecasts/constants.py
@@ -9,5 +9,7 @@ bq_dataset_id = 'test'
 bq_table_id = 'stock_price_predictions'
 start_date = '2014-01-02'
 ticker_list = ['CLS.JO', 'GLN.JO', 'PPH.JO', 'WHL.JO', 'APN.JO', 'RBP.JO', 'PIK.JO', 'PPE.JO',
-               'HIL.JO', 'SOL.JO', 'EXX.JO', 'MCG.JO', 'AIL.JO', 'TGA.JO', 'SSW.JO', 'INL.JO']
+               'HIL.JO', 'SOL.JO', 'EXX.JO', 'MCG.JO', 'AIL.JO', 'TGA.JO', 'SSW.JO', 'INL.JO',
+               'SHP.JO','CFR.JO','FSR.JO','OMU.JO','SLM.JO','RMH.JO','GRT.JO','DSY.JO','GFI.JO',
+               'SNH.JO']
 title = (f":rotating_light: Stock Price Predictions Run:")
