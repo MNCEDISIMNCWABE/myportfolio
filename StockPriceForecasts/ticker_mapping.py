47c0f9fe7b3aec0dc3b74b5a8e7e94c530634110
Mncedisi Mncwabe <67195600+MNCEDISIMNCWABE@users.noreply.github.com>
2023-08-25 08:43:13 +0200

    Update ticker_mapping.py

--- StockPriceForecasts/ticker_mapping.py
+++ StockPriceForecasts/ticker_mapping.py
@@ -18,16 +18,16 @@ def preprocess_ticker_names(for_loop_forecast):
         'TGA.JO': 'Thungela',
         'PPE.JO': 'Purple-Group',
         'SSW.JO': 'Sibanye-Stillwater',
-        'SHP.JO':'Shoprite,
+        'SHP.JO': 'Shoprite',
         'CFR.JO': 'Richemont',
-        'FSR.JO':'FirstRand',
-        'OMU.JO','Old-Mutual',
+        'FSR.JO': 'FirstRand',
+        'OMU.JO': 'Old-Mutual',
         'SLM.JO': 'Sanlam',
-        'RMH.JO':'RMB',
-        'GRT.JO':'Growth-Point',
-        'DSY.JO':'Discovery',
-        'GFI.JO':'Gold-Fields',
-        'SNH.JO':'Steinhoff'
+        'RMH.JO': 'RMB',
+        'GRT.JO': 'Growth-Point',
+        'DSY.JO': 'Discovery',
+        'GFI.JO': 'Gold-Fields',
+        'SNH.JO': 'Steinhoff'
     }
 
     for_loop_forecast['ticker_name'] = for_loop_forecast['ticker'].map(ticker_name_mapping)
