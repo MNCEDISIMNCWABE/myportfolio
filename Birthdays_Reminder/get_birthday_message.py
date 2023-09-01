aa064e409dec1905a672722372c96a647358e089
Mncedisi Mncwabe <67195600+MNCEDISIMNCWABE@users.noreply.github.com>
2023-09-02 00:49:45 +0200

    Add files via upload

---
+++ Birthdays_Reminder/get_birthday_message.py
@@ -0,0 +1,3 @@
+def get_birthday_message(name: str, today: str) -> str:
+    if birthdays.get(name) == today:
+        return f":tada: Happy Birthday, {name}! :tada: Sending you best wishes on your special day! :confetti_ball:"
