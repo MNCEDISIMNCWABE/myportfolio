640580428c2768bb77879b0f04a941e2a46c7c3e
Mncedisi Mncwabe <67195600+MNCEDISIMNCWABE@users.noreply.github.com>
2023-09-02 00:55:27 +0200

    Update constants.py

--- Birthdays_Reminder/constants.py
+++ Birthdays_Reminder/constants.py
@@ -1,8 +1,7 @@
 # Send message to slack
 slack_credentials = 'https://hooks.slack.com/services/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'
 
-people_birthdays = 
-{
+people_birthdays = {
   'Mncedisi': '13-09',
   'Mvikeli': '09-07',
   'Mom': '02-09',
