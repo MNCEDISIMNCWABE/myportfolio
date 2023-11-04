1ab9271ba24484d478ec128e481ae507b0f0b822
MNCEDISIMNCWABE <MNCEDISIMNCWABE>
2023-11-04 21:47:44 +0000

    [Changed] search query to toast makers

--- PriceTracker/constants.py
+++ PriceTracker/constants.py
@@ -1,4 +1,4 @@
 incredible_endpoint = 'https://www.incredible.co.za/'
-search_query = 'products/appliances-new-9/laundry/front-loader-washing-machines'
+search_query = 'products/appliances-new-9/toasters-sandwich-makers'
 slack_credentials = 'https://hooks.slack.com/services/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'
 title = ":rotating_light: Price Tracker Run:"
