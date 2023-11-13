5db93da00785f8ed616543f35ace1451a70124b2
Mncedisi Mncwabe <67195600+MNCEDISIMNCWABE@users.noreply.github.com>
2023-11-13 20:26:48 +0200

    Update constants.py

--- PriceTracker/constants.py
+++ PriceTracker/constants.py
@@ -1,4 +1,4 @@
 incredible_endpoint = 'https://www.incredible.co.za/'
-search_query = 'products/appliances-new-9/toasters-sandwich-makers'
+search_query = 'swan-red-retro-two-slice-toaster-st01r'
 slack_credentials = 'https://hooks.slack.com/services/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'
 title = ":rotating_light: Price Tracker Run:"
