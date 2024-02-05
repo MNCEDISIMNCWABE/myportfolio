c6c521c4efbd0bcf5bcafc95e4d295afdb3f6307
Mncedisi Mncwabe <67195600+MNCEDISIMNCWABE@users.noreply.github.com>
2024-02-05 18:23:12 +0200

    Update lotto_predictions.py

--- lottery_predictions/lotto_predictions.py
+++ lottery_predictions/lotto_predictions.py
@@ -1,8 +1,6 @@
 import pandas as pd
 import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
-import requests, bs4
+import requests
 import datetime as dt
 import os
 import random
