e0827ceb1aa0fc2ebcb8cefc929f94b2dff45dda
Mncedisi Mncwabe <3621029@myuwc.ac.za>
2024-04-13 23:05:39 +0200

    Updated query

--- PriceTracker/constants.py
+++ PriceTracker/constants.py
@@ -1,4 +1,4 @@
 incredible_endpoint = 'https://www.incredible.co.za/'
-search_query = 'products/cellphones-wearables/wearable-technology/watches'
+search_query = 'products/cellphones-wearables/wearable-technology/fitness-trackers'
 slack_credentials = 'https://hooks.slack.com/services/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'
 title = ":rotating_light: Price Tracker Run:"
