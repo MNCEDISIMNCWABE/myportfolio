73410ca7c7829f2b10ae061e36d56222308bf33e
Mncedisi Mncwabe <3621029@myuwc.ac.za>
2024-05-21 17:12:11 +0200

    add new query

--- All_Files/Crypto/marketing_costs_table.sql
+++ All_Files/Crypto/marketing_costs_table.sql
@@ -35,3 +35,70 @@ FROM (
 GROUP BY date, cost_type;
 
 ALTER VIEW analytics.bitx.vw_marketing_costs owner TO `schema_owners`;
+
+
+--- V2 with user_id
+CREATE OR REPLACE VIEW analytics.bitx.vw_marketing_costs AS
+SELECT
+  user_id,
+  created_at,
+  preferred_locale,
+  provider,
+  cost_type,
+  base_cost_currency,
+  SUM(cost) AS cost,
+  SUM(cost_usd) AS cost_usd
+FROM (
+  SELECT
+    r.user_id,
+    DATE(r.applied_at) AS created_at,
+    u.preferred_locale,
+    'Luno' AS provider,
+    'referral' AS cost_type,
+    r.currency AS base_cost_currency,
+    COALESCE(r.amount, 0) AS cost,
+    COALESCE(r.reward_amount_usd, 0) AS cost_usd
+  FROM analytics.bitx.referrals r
+  LEFT JOIN analytics.bitx.users u
+       ON r.user_id = u.id
+  UNION ALL
+  SELECT
+    ref.user_id,
+    DATE(ref.applied_at) AS created_at,
+    u.preferred_locale,
+    'Luno' AS provider,
+    'referrer' AS cost_type,
+    ref.rr_currency AS base_cost_currency,
+    COALESCE(ref.rr_amount, 0) AS cost,
+    COALESCE(ref.rr_reward_amount_usd, 0) AS cost_usd
+  FROM analytics.bitx.referrers ref
+  LEFT JOIN analytics.bitx.users u
+       ON ref.user_id = u.id
+  UNION ALL
+  SELECT
+    p.user_id,
+    DATE(p.created_at) AS created_at,
+    u.preferred_locale,
+    'Luno' AS provider,
+    'promo' AS cost_type,
+    p.currency AS base_cost_currency,
+    COALESCE(p.amount, 0) AS cost,
+    COALESCE(p.amount_usd, 0) AS cost_usd
+  FROM analytics.bitx.promos p
+  LEFT JOIN analytics.bitx.users u
+       ON p.user_id = u.id
+  UNION ALL
+  SELECT
+    NULL AS user_id,
+    DATE(campaign_date) AS created_at,
+    country_field AS preferred_locale,
+    'Luno' AS provider,
+    'paid_marketing' AS cost_type,
+    adn_original_currency AS base_cost_currency,
+    COALESCE(adn_original_cost, 0) AS cost,
+    COALESCE(adn_cost, 0) AS cost_usd
+  FROM analytics.singular.vw_singular_campaigns
+) consolidated_costs
+GROUP BY user_id, created_at, preferred_locale, provider, cost_type, base_cost_currency;
+
+ALTER VIEW analytics.bitx.vw_marketing_costs owner TO `schema_owners`;
