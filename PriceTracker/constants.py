e577576e769aa7c4f8986367244cf9325b9f8abb
Mncedisi Mncwabe <67195600+MNCEDISIMNCWABE@users.noreply.github.com>
2024-06-13 14:03:07 +0200

    Update constants.py

--- PriceTracker/constants.py
+++ PriceTracker/constants.py
@@ -1,4 +1,4 @@
 incredible_endpoint = 'https://www.incredible.co.za/'
 search_query = 'products/cellphones-wearables/wearable-technology/fitness-trackers'
-slack_credentials = 'https://hooks.slack.com/services/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'
+slack_credentials = 'https://hooks.slack.com/services/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'
 title = ":rotating_light: Price Tracker Run:"
