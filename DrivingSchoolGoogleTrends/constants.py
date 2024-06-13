84cd398c192d72624caf912dba702b89a3f97707
Mncedisi Mncwabe <67195600+MNCEDISIMNCWABE@users.noreply.github.com>
2024-06-13 14:07:53 +0200

    Update constants.py

--- DrivingSchoolGoogleTrends/constants.py
+++ DrivingSchoolGoogleTrends/constants.py
@@ -1,5 +1,5 @@
 from google.oauth2.service_account import Credentials
-slack_credentials = 'https://hooks.slack.com/services/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'
+slack_credentials = 'https://hooks.slack.com/services/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'
 title = (f":rotating_light: Driving School Search Terms - Google Trends Past 24 hours:")
 # Set the keywords and time frame for the query
 kw_list = ["driving school", "learners license", "driving license", "driving schools near me", "code 14 driving school"]
